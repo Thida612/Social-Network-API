f4c50ec168cd2e089f63986354de2cca21198d93
Thida612 <angelic722@gmail.com>
2025-01-29 19:19:09 -0600

    Set the port for the server

---
+++ config/connection.js
@@ -0,0 +1,7 @@
+// Import necessary modules
+const express = require("express");
+const db = require("./config/connection");
+const routes = require("./routes");
+
+// Set the port for the server
+const PORT = process.env.PORT || 3001;
