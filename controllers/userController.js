2d55019d1f8a222c183a101620e503291307a690
Thida612 <angelic722@gmail.com>
2025-01-29 20:07:17 -0600

    Add addFriend function to add a friend to a user's friend list

--- controllers/userController.js
+++ controllers/userController.js
@@ -41,3 +41,17 @@ module.exports = {
       .then(() => res.json({ message: "User and Thought deleted!" }))
       .catch((err) => res.status(500).json(err));
   },
+  //add a friend
+  addFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { friends: req.params.friendId } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No User find with this ID!" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
