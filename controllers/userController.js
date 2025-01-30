ee518cf325728fc3d27d97372477fe321a820a93
Thida612 <angelic722@gmail.com>
2025-01-29 19:59:03 -0600

    Add getSingleUser function to fetch a single user by ID

---
+++ controllers/userController.js
@@ -0,0 +1,22 @@
+const { User, Thought } = require("../models");
+
+module.exports = {
+  //Get all users
+  getUser(req, res) {
+    User.find({})
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+   //Get single user
+   getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .populate("thoughts")
+      .populate("friends")
+      .select("-__v")
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No User find with that ID!" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
