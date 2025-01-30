0181f0fe3b75b8d184a0f387abd8387bf458476b
Thida612 <angelic722@gmail.com>
2025-01-29 19:26:18 -0600

    add routes defined in the routes folder and start the server once the database connection is open

--- config/connection.js
+++ config/connection.js
@@ -10,3 +10,12 @@ const app = express();
 // Middleware to parse URL-encoded data and JSON data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// Use the routes defined in the routes folder
+app.use(routes);a
+
+// Start the server once the database connection is open
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
